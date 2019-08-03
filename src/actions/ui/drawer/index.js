276e73ad29b01eb02e731a832543e2c0f0f55f80
Bomavi <max.bozh.n1mbo@gmail.com>
2019-08-04 02:00:04 +0300

    Modify ui actions

--- src/actions/ui/drawer/index.js
+++ src/actions/ui/drawer/index.js
@@ -1,7 +1,7 @@
 /* local imports: common */
 import * as types from './../types';
 
-export const toggleDrawer = (isOpen = null) => ({
+export const toggleDrawer = (payload = null) => ({
 	type: types.TOGGLE_DRAWER,
-	payload: isOpen,
+	payload,
 });
