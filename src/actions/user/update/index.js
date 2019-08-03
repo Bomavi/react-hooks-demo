c67c4c47cf38e669bb01ba28814a821ab2068ee7
Bomavi <max.bozh.n1mbo@gmail.com>
2019-08-04 02:00:16 +0300

    Add user thunk actions

--- src/actions/user/update/index.js
+++ src/actions/user/update/index.js
@@ -1,14 +1,13 @@
 /* local imports: common */
 import * as types from './../types';
 
-export const updateUser = user => ({
-	type: types.UPDATE_USER,
-	payload: user,
-});
+/* root imports: common */
+import { services } from 'config/services';
+import { setThemeInProgress } from 'actions/theme';
 
-export const updateUserOnSuccess = user => ({
+export const updateUserOnSuccess = payload => ({
 	type: types.UPDATE_USER_ON_SUCCESS,
-	payload: user,
+	payload,
 });
 
 export const updateUserOnFail = error => ({
@@ -16,3 +15,22 @@ export const updateUserOnFail = error => ({
 	payload: error,
 	error: true,
 });
+
+export const updateUser = payload => async (dispatch, getState) => {
+	dispatch(setThemeInProgress(true));
+
+	try {
+		const state = getState();
+		const currentUser = state.auth.user;
+
+		if (!currentUser) throw new Error('no user found');
+
+		const updatedUser = await services.api.users.update(currentUser._id, payload);
+
+		dispatch(updateUserOnSuccess(updatedUser));
+	} catch (e) {
+		dispatch(updateUserOnFail(e.message));
+	} finally {
+		dispatch(setThemeInProgress(false));
+	}
+};
