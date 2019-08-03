b2c138683aefe13e979debe659c9583d8995d9e2
Bomavi <max.bozh.n1mbo@gmail.com>
2019-08-03 19:51:57 +0300

    Add root

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,18 @@
-// import React from 'react';
+/* npm imports: common */
+import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 
-render('Hello!', document.getElementById('root'));
+/* root imports: view components */
+import { App } from 'views/layouts/App';
+
+/* root imports: common */
+import { store } from 'store';
+
+/* start react app */
+render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
