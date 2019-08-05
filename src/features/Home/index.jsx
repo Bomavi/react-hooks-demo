499ab8555c4e9f2dbc73dc8dc36fe55f0f5be640
Bomavi <max.bozh.n1mbo@gmail.com>
2019-08-05 19:24:34 +0300

    Add features/Home root

---
+++ src/features/Home/index.jsx
@@ -0,0 +1,18 @@
+/* npm imports: common */
+import React from 'react';
+
+/* npm imports: material-ui/core */
+import Container from '@material-ui/core/Container';
+
+/* local imports: common */
+import { TaskList } from './containers';
+import { ActionTabs } from './components';
+
+const Home = React.memo(() => (
+	<Container maxWidth="md">
+		<ActionTabs />
+		<TaskList />
+	</Container>
+));
+
+export { Home };
