7327eb1f59cd8238dc646863638dea2c07af2db7
Bomavi <max.bozh.n1mbo@gmail.com>
2019-08-05 18:42:35 +0300

    Add features/Home/components/Search

---
+++ src/features/Home/components/Search/index.jsx
@@ -0,0 +1,55 @@
+/* npm imports: common */
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+/* root imports: view components */
+import { CustomInput } from 'views/elements';
+
+/* root imports: common */
+import { debounce, debounceTiming } from 'utils/helpers';
+import { searchTasks, fetchTasks } from 'actions/tasks';
+
+/* local imports: common */
+// import { useStyles } from './styles';
+
+const Search = React.memo(() => {
+	// const classes = useStyles();
+	const dispatch = useDispatch();
+
+	const isFetching = useSelector(state => state.tasks.isFetching);
+
+	const changeHandler = React.useCallback(
+		debounce(value => {
+			searchHandler(value);
+		}, debounceTiming.input),
+		[]
+	);
+
+	const searchHandler = React.useCallback(
+		payload => {
+			dispatch(searchTasks(payload));
+			dispatch(fetchTasks());
+		},
+		[dispatch]
+	);
+
+	React.useEffect(() => {
+		return () => {
+			searchHandler('');
+		};
+	}, [searchHandler]);
+
+	return (
+		<CustomInput
+			icon={{
+				name: 'magnify',
+				svgSize: 'md',
+			}}
+			isFetching={isFetching}
+			placeholder="Type to search..."
+			onChange={changeHandler}
+		/>
+	);
+});
+
+export { Search };
