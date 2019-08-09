78b56eb67e133f2084ef102de8ded3fb5a3c0ee2
Bomavi <max.bozh.n1mbo@gmail.com>
2019-08-10 00:16:43 +0300

    Add task mocked data

---
+++ src/__mocks__/data/task.js
@@ -0,0 +1,13 @@
+const taskFabric = index => {
+	return {
+		_id: `t_id_${index}`,
+		description: `Description Num ${index}`,
+		completed: false,
+		createdAt: 'some date',
+		updatedAt: 'some date',
+	};
+};
+
+export const task = taskFabric(1);
+
+export const tasks = [1, 2, 3].map(i => taskFabric(i));
