3d98c63b6178693f510d271add20939f67cbd469
Bomavi <max.bozh.n1mbo@gmail.com>
2019-08-10 00:21:09 +0300

    Add authenticate thunk tests

---
+++ src/actions/auth/authenticate/index.test.js
@@ -0,0 +1,51 @@
+/* mock imports: common */
+import { user } from '__mocks__/data';
+import { mockStore } from '__mocks__/store';
+import { getInitialState } from '__mocks__/state';
+import { mockedAuthServerResponse } from '__mocks__/services/auth';
+
+/* root imports: common */
+import { setIsInitialized } from 'actions/auth';
+import * as types from 'actions/auth/types';
+
+/* local imports: common */
+import { authenticate, authenticateOnSuccess, authenticateOnFail } from '.';
+
+const initialState = getInitialState();
+const store = mockStore(initialState);
+
+describe('Dispatch thunk: AUTHENTICATE', () => {
+	afterEach(() => {
+		mockedAuthServerResponse.reset();
+		store.clearActions();
+	});
+
+	afterAll(() => {
+		mockedAuthServerResponse.restore();
+	});
+
+	test(types.AUTHENTICATE_ON_FAIL, async () => {
+		mockedAuthServerResponse.initFailResponse();
+
+		await store.dispatch(authenticate());
+
+		const actions = store.getActions();
+		const toEqual = [
+			authenticateOnFail('Error: Network Error'),
+			setIsInitialized(true),
+		];
+
+		expect(actions).toEqual(toEqual);
+	});
+
+	test(types.AUTHENTICATE_ON_SUCCESS, async () => {
+		mockedAuthServerResponse.initSuccessResponse();
+
+		await store.dispatch(authenticate());
+
+		const actions = store.getActions();
+		const toEqual = [authenticateOnSuccess(user), setIsInitialized(true)];
+
+		expect(actions).toEqual(toEqual);
+	});
+});
