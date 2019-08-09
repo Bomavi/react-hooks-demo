9e086a9b45f563f1ebdfcc5f4240afc8ce7e16b1
Bomavi <max.bozh.n1mbo@gmail.com>
2019-08-10 00:22:18 +0300

    Add updateTask thunk tests

---
+++ src/actions/tasks/update/index.test.js
@@ -0,0 +1,61 @@
+/* mock imports: common */
+import { task } from '__mocks__/data';
+import { mockStore } from '__mocks__/store';
+import { getInitialState } from '__mocks__/state';
+import { mockedTasksServerResponse } from '__mocks__/services/tasks';
+
+/* root imports: common */
+import { setUpdateInProgress } from 'actions/tasks';
+import * as types from 'actions/tasks/types';
+
+/* local imports: common */
+import { updateTask, updateTaskOnSuccess, updateTaskOnFail } from '.';
+
+const initialState = getInitialState();
+const store = mockStore(initialState);
+const taskId = task._id;
+const data = {
+	description: task.description,
+	completed: task.completed,
+};
+
+describe('Dispatch thunk: UPDATE_TASK', () => {
+	afterEach(() => {
+		mockedTasksServerResponse.reset();
+		store.clearActions();
+	});
+
+	afterAll(() => {
+		mockedTasksServerResponse.restore();
+	});
+
+	test(types.UPDATE_TASK_ON_FAIL, async () => {
+		mockedTasksServerResponse.initFailResponse();
+
+		await store.dispatch(updateTask(taskId, data));
+
+		const actions = store.getActions();
+		const toEqual = [
+			setUpdateInProgress(taskId, true),
+			updateTaskOnFail('Error: Network Error'),
+			setUpdateInProgress(taskId, false),
+		];
+
+		expect(actions).toEqual(toEqual);
+	});
+
+	test(types.UPDATE_TASK_ON_SUCCESS, async () => {
+		mockedTasksServerResponse.initSuccessResponse();
+
+		await store.dispatch(updateTask(taskId, data));
+
+		const actions = store.getActions();
+		const toEqual = [
+			setUpdateInProgress(taskId, true),
+			updateTaskOnSuccess(task),
+			setUpdateInProgress(taskId, false),
+		];
+
+		expect(actions).toEqual(toEqual);
+	});
+});
