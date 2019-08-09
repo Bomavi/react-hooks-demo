fd50ae9b819e570ee22cfb26b1c1f4217a8d7c7f
Bomavi <max.bozh.n1mbo@gmail.com>
2019-08-10 00:18:38 +0300

    Add mocked redux store

---
+++ src/__mocks__/store/index.js
@@ -0,0 +1,7 @@
+/* npm imports: common */
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+const middleware = [thunk];
+
+export const mockStore = configureStore(middleware);
