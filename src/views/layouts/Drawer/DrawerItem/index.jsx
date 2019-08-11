e4b803e08ff5274dde8c8a86df688b0c8bb8e9f1
Bomavi <max.bozh.n1mbo@gmail.com>
2019-08-12 01:43:09 +0300

    Add propTypes and defaultProps

--- src/views/layouts/Drawer/DrawerItem/index.jsx
+++ src/views/layouts/Drawer/DrawerItem/index.jsx
@@ -1,5 +1,6 @@
 /* npm imports: common */
 import React from 'react';
+import PropTypes from 'prop-types';
 
 /* npm imports: material-ui/core */
 import CircularProgress from '@material-ui/core/CircularProgress';
@@ -10,19 +11,32 @@ import ListItemText from '@material-ui/core/ListItemText';
 /* root imports: view components */
 import { Icon } from 'views/elements';
 
-const DrawerItem = React.memo(
-	({ iconName, text, caption, inProgress = false, onClick }) => (
-		<ListItem button onClick={onClick}>
-			<ListItemIcon>
-				{inProgress ? (
-					<CircularProgress size={18} thickness={4} color="inherit" />
-				) : (
-					<Icon name={iconName} svgSize="md" />
-				)}
-			</ListItemIcon>
-			<ListItemText primary={text} secondary={caption} />
-		</ListItem>
-	)
-);
+/* root imports: common */
+import { iconNames } from 'utils/shapes';
+
+const DrawerItem = React.memo(({ iconName, text, caption, inProgress, onClick }) => (
+	<ListItem button onClick={onClick}>
+		<ListItemIcon>
+			{inProgress ? (
+				<CircularProgress size={18} thickness={4} color="inherit" />
+			) : (
+				<Icon name={iconName} svgSize="md" />
+			)}
+		</ListItemIcon>
+		<ListItemText primary={text} secondary={caption} />
+	</ListItem>
+));
+
+DrawerItem.propTypes = {
+	iconName: PropTypes.oneOf(iconNames),
+	text: PropTypes.string,
+	caption: PropTypes.string,
+	inProgress: PropTypes.bool,
+	onClick: PropTypes.func,
+};
+
+DrawerItem.defaultProps = {
+	inProgress: false,
+};
 
 export { DrawerItem };
