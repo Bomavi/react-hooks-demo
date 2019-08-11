9a8c6c71d4327dacaf83d9c8b2fb41d99af01b9b
Bomavi <max.bozh.n1mbo@gmail.com>
2019-08-12 01:44:17 +0300

    Add propTypes and defaultProps

--- src/features/Home/components/TaskCheckbox/index.jsx
+++ src/features/Home/components/TaskCheckbox/index.jsx
@@ -1,5 +1,6 @@
 /* npm imports: common */
 import React from 'react';
+import PropTypes from 'prop-types';
 
 /* npm imports: material-ui/core */
 import Checkbox from '@material-ui/core/Checkbox';
@@ -8,7 +9,7 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 /* local imports: common */
 import { useStyles } from './styles';
 
-const TaskCheckbox = React.memo(({ value, disabled, onChange, isFetching = false }) => {
+const TaskCheckbox = React.memo(({ value, disabled, onChange, isFetching }) => {
 	const classes = useStyles();
 
 	if (isFetching) {
@@ -31,5 +32,16 @@ const TaskCheckbox = React.memo(({ value, disabled, onChange, isFetching = false
 		</div>
 	);
 });
+
+TaskCheckbox.propTypes = {
+	value: PropTypes.bool.isRequired,
+	disabled: PropTypes.bool,
+	isFetching: PropTypes.bool,
+	onChange: PropTypes.func.isRequired,
+};
+
+TaskCheckbox.defaultProps = {
+	isFetching: false,
+};
 
 export { TaskCheckbox };
