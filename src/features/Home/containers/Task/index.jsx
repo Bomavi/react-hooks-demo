632727aad816b5243fd89b91d75203de0a6d4204
Bomavi <max.bozh.n1mbo@gmail.com>
2019-08-12 01:44:47 +0300

    Add propTypes and defaultProps, fix mouseLeave

--- src/features/Home/containers/Task/index.jsx
+++ src/features/Home/containers/Task/index.jsx
@@ -1,5 +1,6 @@
 /* npm imports: common */
 import React, { useState, useCallback } from 'react';
+import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import cx from 'classnames';
 
@@ -11,6 +12,7 @@ import { TaskCheckbox, TaskActions } from 'features/Home/components';
 
 /* root imports: common */
 import { updateTask, deleteTask } from 'actions/tasks';
+import { TaskShape } from 'utils/shapes';
 
 /* local imports: common */
 import { Description } from './Description';
@@ -18,7 +20,7 @@ import { EditInput } from './EditInput';
 import { Backdrop } from './Backdrop';
 import { useStyles } from './styles';
 
-const Task = React.memo(({ task, isLastChild = false }) => {
+const Task = React.memo(({ task, isLastChild }) => {
 	const classes = useStyles();
 	const dispatch = useDispatch();
 
@@ -40,6 +42,7 @@ const Task = React.memo(({ task, isLastChild = false }) => {
 
 	const completeHandler = useCallback(() => {
 		dispatch(updateTask(task._id, { ...task, completed: !task.completed }));
+		mouseLeaveHandler();
 	}, [dispatch, task]);
 
 	const saveHandler = useCallback(
@@ -65,7 +68,7 @@ const Task = React.memo(({ task, isLastChild = false }) => {
 				onChange={completeHandler}
 			/>
 			<Divider className={classes.divider} />
-			<Description>{task.description}</Description>
+			<Description completed={task.completed}>{task.description}</Description>
 			{isHovered && !task.deleteInProgress && (
 				<Divider className={classes.divider} />
 			)}
@@ -89,5 +92,14 @@ const Task = React.memo(({ task, isLastChild = false }) => {
 		</div>
 	);
 });
+
+Task.propTypes = {
+	task: PropTypes.shape(TaskShape).isRequired,
+	isLastChild: PropTypes.bool.isRequired,
+};
+
+Task.defaultProps = {
+	isLastChild: false,
+};
 
 export { Task };
